2cd924ad6cf2d520a414eec0b474d67a118bb48c
Salvador Guzman <guzmansalv@gmail.com>
2017-12-09 14:26:10 -0800

    grab user and pass

--- src/main/scala/com/github/sguzman/scala/uber/login/Main.scala
+++ src/main/scala/com/github/sguzman/scala/uber/login/Main.scala
@@ -1,11 +1,19 @@
 package com.github.sguzman.scala.uber.login
 
+import org.feijoas.mango.common.base.Preconditions
+
 import scala.util.{Failure, Success}
 
 object Main {
   def main(args: Array[String]): Unit = {
     util.Try({
-      println("Hello world")
+      val user = System.getenv("USERNAME")
+      val pass = System.getenv("PASSWORD")
+
+      Preconditions.checkNotNull(user)
+      Preconditions.checkNotNull(pass)
+
+      println("hello world")
     }) match {
       case Success(_) => println("Done")
       case Failure(e) => Console.err.println(e)
