9b294b0d5fa55e52c71f40d3a88e80ef61eb9f08
Salvador Guzman <guzmansalv@gmail.com>
2017-12-09 15:01:38 -0800

    move common retorts into method calls

--- src/main/scala/com/github/sguzman/scala/uber/login/Main.scala
+++ src/main/scala/com/github/sguzman/scala/uber/login/Main.scala
@@ -4,23 +4,15 @@ import com.github.sguzman.scala.uber.login.typesafe.email.{Answer, Email, UserId
 import org.feijoas.mango.common.base.Preconditions
 
 import scala.util.{Failure, Success}
-import scalaj.http.Http
-
+import scalaj.http.{Http, HttpResponse}
 import io.circe.syntax._
 import io.circe.generic.auto._
 
 object Main {
   def main(args: Array[String]): Unit = {
     util.Try({
-      val user = System.getenv("USERNAME")
-      val pass = System.getenv("PASSWORD")
-
-      Preconditions.checkNotNull(user)
-      Preconditions.checkNotNull(pass)
-
-      val loginPageURL = "https://auth.uber.com/login/?next_url=https%3A%2F%2Fpartners.uber.com"
-      val request = Http(loginPageURL)
-      val response = request.asString
+      val (user, pass) = getCreds
+      val response = getLoginPage
 
       val postURL = "https://auth.uber.com/login/handleanswer"
       val payload = Email(Answer(`type` = "VERIFY_INPUT_USERNAME", UserIdentifier(user)),  init = true)
@@ -38,5 +30,18 @@ object Main {
       case Success(_) => println("Done")
       case Failure(e) => Console.err.println(e)
     }
+  }
+
+  def getLoginPage: HttpResponse[String] =
+    Http("https://auth.uber.com/login/?next_url=https%3A%2F%2Fpartners.uber.com")
+      .asString
+
+  def getCreds: (String, String) = {
+    val user = System.getenv("USERNAME")
+    val pass = System.getenv("PASSWORD")
+
+    Preconditions.checkNotNull(user)
+    Preconditions.checkNotNull(pass)
+    (user, pass)
   }
 }
