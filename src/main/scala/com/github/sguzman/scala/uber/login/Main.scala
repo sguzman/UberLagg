47d252540d95e4ad345cd2d7fa20ea4e5433cb09
Salvador Guzman <guzmansalv@gmail.com>
2017-12-09 14:21:56 -0800

    add error printing

--- src/main/scala/com/github/sguzman/scala/uber/login/Main.scala
+++ src/main/scala/com/github/sguzman/scala/uber/login/Main.scala
@@ -1,7 +1,14 @@
 package com.github.sguzman.scala.uber.login
 
+import scala.util.{Failure, Success}
+
 object Main {
   def main(args: Array[String]): Unit = {
-    println("hello, world")
+    util.Try({
+      println("Hello world")
+    }) match {
+      case Success(_) => println("Done")
+      case Failure(e) => Console.err.println(e)
+    }
   }
 }
