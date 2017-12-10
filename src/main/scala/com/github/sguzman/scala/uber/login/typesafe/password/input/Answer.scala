c83bd6200fd562118838c089818bd03e8cca864d
Salvador Guzman <guzmansalv@gmail.com>
2017-12-09 18:42:12 -0800

    added typesafe stuff

---
+++ src/main/scala/com/github/sguzman/scala/uber/login/typesafe/password/input/Answer.scala
@@ -0,0 +1,6 @@
+package com.github.sguzman.scala.uber.login.typesafe.password.input
+
+case class Answer(
+                 password: String,
+                 `type`: String
+                 )
---
+++ src/main/scala/com/github/sguzman/scala/uber/login/typesafe/password/input/Password.scala
@@ -0,0 +1,6 @@
+package com.github.sguzman.scala.uber.login.typesafe.password.input
+
+case class Password(
+                   answer: Answer,
+                   rememberMe: Boolean
+                   )
